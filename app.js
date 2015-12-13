1de3441b132acf35bf48d2d4df1573f1318d7e06
Jeremy Shore <xboxmusicn3rd@gmail.com>
2015-12-13 01:53:54 -0600

    Initial Push

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require('express'),
+    app = express(),
+    server = require('http').Server(app),
+    io = require('socket.io')(server);
+
+_attachEvents();
+app.use('/actions', express.static(__dirname + '/pages/actions'));
+    
+function _attachEvents() {
+    io.of('/actions')
+        .on('connection', clientConnect);
+}
+
+function clientConnect(socket) {
+    console.log('New Connection Established...');
+    
+    socket.on('action fired', actionRecieved);
+}
+
+function actionRecieved(content) {
+    var action = JSON.parse(content);
+    
+    
+}
+
+function broadcast(message) {
+    io.sockets.emit(message);
+}
+
+server.listen(8080);
